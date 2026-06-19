bf27d10c977dbf31e46a9ee67c20a12bbac8f8df
bajon1 <dmika@student.agh.edu.pl>
2026-06-19 08:37:33 +0200

    slight make_loaders script refactoring

--- Lab12/utils/data.py
+++ Lab12/utils/data.py
@@ -2,20 +2,23 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 
-def make_loaders(X, y, batch_size, test_size = 0.2):
+def make_loaders(X, y, batch_size, test_size=0.2, squeeze_y=False):
     X_trainval, X_test, y_trainval, y_test = train_test_split(X, y, test_size=test_size)
-    X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, test_size=test_size)
+    X_train, X_val, y_train, y_val         = train_test_split(X_trainval, y_trainval, test_size=test_size)
 
-    train_loader = _make_loader(X_train, y_train, batch_size, shuffle=True)
-    val_loader = _make_loader(X_val, y_val, batch_size, shuffle=False)
-    test_loader = _make_loader(X_test, y_test, batch_size, shuffle=False)
+    train_loader = _make_loader(X_train, y_train, batch_size, shuffle=True,  squeeze_y=squeeze_y)
+    val_loader   = _make_loader(X_val,   y_val,   batch_size, shuffle=False, squeeze_y=squeeze_y)
+    test_loader  = _make_loader(X_test,  y_test,  batch_size, shuffle=False, squeeze_y=squeeze_y)
 
     return train_loader, val_loader, test_loader, X_train, y_train
 
 
-def _make_loader(X, y, batch_size, shuffle = False):
+def _make_loader(X, y, batch_size, shuffle=False, squeeze_y=False):
     X_t = torch.from_numpy(X)
-    y_t = torch.from_numpy(y).unsqueeze(1)
+    y_t = torch.from_numpy(y)
+
+    if not squeeze_y:
+        y_t = y_t.unsqueeze(1)
 
     dataset = TensorDataset(X_t, y_t)
     return DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
