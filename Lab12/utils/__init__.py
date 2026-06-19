6b6f2dcdb7fa38400416488a4af128eb1deda14a
bajon1 <dmika@student.agh.edu.pl>
2026-06-19 09:10:20 +0200

    lab 12 task 2 evaluation done

--- Lab12/utils/__init__.py
+++ Lab12/utils/__init__.py
@@ -2,4 +2,4 @@ from .setup import set_seed, generate_correlated_regression_data, make_imbalance
 from .data import make_loaders
 from .models import BaseRegressionNet, MTLAutoencoderRegressor, BinaryFocalLoss, BinaryClassifierMLP
 from .train import train_baseline, train_mtl
-from .eval import compute_clf_metrics
+from .eval import compute_clf_metrics, get_probs
--- Lab12/utils/eval.py
+++ Lab12/utils/eval.py
@@ -1,4 +1,19 @@
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score, average_precision_score
+import torch
+import numpy as np
+
+
+def get_probs(model, loader, device='cpu'):
+    model.eval()
+    y_true, y_prob = [], []
+    with torch.no_grad():
+        for X_batch, y_batch in loader:
+            logits = model(X_batch.to(device)).cpu()
+            y_prob.extend(torch.sigmoid(logits).numpy())
+            y_true.extend(y_batch.numpy())
+    return np.array(y_true), np.array(y_prob)
+
+
 
 def compute_clf_metrics(y_true, y_prob, threshold=0.5):
     y_pred = (y_prob >= threshold).astype(int)
